71a53433c49eecc8338dd53ad7daf4efdd4df725
kierenblack <dunnersguy@gmail.com>
2018-11-04 14:54:16 +1300

    Build room array

--- presentation/Program.cs
+++ presentation/Program.cs
@@ -8,16 +8,48 @@ namespace presentation
 {
     class Program
     {
-        public struct FileList
+        public struct Direction
         {
-            public string fName;
-            public string lName;
-            public string interest;
+            public string room;
+            public bool up;
+            public bool down;
+            public bool right;
+            public bool left;
         }
 
         static void Main()
         {
+            Direction[] direction = new Direction[] //create empty array to pass through with ref
+            {
+               new Direction(){ room = "", up = false, down = false, right = false, left = false }
+            };
+            buildArrays(ref direction);
+        }
 
+        static void buildArrays(ref Direction[] direction)
+        {
+
+            direction = new Direction[] //create empty array to pass through with ref
+            {
+               new Direction(){ room = "A1", up = false, down = false, right = true, left = false },
+               new Direction(){ room = "A2", up = false, down = true, right = false, left = false },
+               new Direction(){ room = "A3", up = true, down = true, right = true, left = false },
+               new Direction(){ room = "A4", up = true, down = false, right = false, left = false },
+               new Direction(){ room = "B1", up = false, down = false, right = true, left = true },
+               new Direction(){ room = "B2", up = false, down = true, right = true, left = false },
+               new Direction(){ room = "B3", up = true, down = true, right = false, left = true },
+               new Direction(){ room = "B4", up = true, down = false, right = true, left = false },
+               new Direction(){ room = "C1", up = false, down = false, right = true, left = true },
+               new Direction(){ room = "C2", up = false, down = false, right = true, left = true },
+               new Direction(){ room = "C3", up = false, down = true, right = false, left = false },
+               new Direction(){ room = "C4", up = true, down = false, right = true, left = true },
+               new Direction(){ room = "D1", up = false, down = true, right = false, left = true },
+               new Direction(){ room = "D2", up = true, down = true, right = false, left = true },
+               new Direction(){ room = "D3", up = true, down = true, right = false, left = false },
+               new Direction(){ room = "D4", up = true, down = false, right = false, left = true },
+
+            };
+            Console.ReadLine();
         }
     }
 }
